cc5d242c529308f610231e4032ed9edec82db7c7
rcc <rcc>
2017-04-18 12:48:50 -0700

    Signed-off-by: rcc <rcc>

---
+++ Proj/CSC17A_Project/Mode.h
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Mode.h
+ * Author: Professor
+ *
+ * Created on April 13, 2017, 10:13 AM
+ */
+
+#ifndef MODE_H
+#define MODE_H
+
+struct Mode {
+    int row; //Size of rows
+    int col; //Size of columns
+};
+
+struct UsrInfo {
+    char name[81]; //Array to store name
+    int totScor; //Stores total score
+};
+
+#endif /* MODE_H */
+
---
+++ Proj/Previous Versions/CSC17A_Project_Version1.2/Mode.h
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Mode.h
+ * Author: Professor
+ *
+ * Created on April 13, 2017, 10:13 AM
+ */
+
+#ifndef MODE_H
+#define MODE_H
+
+struct Mode {
+    int row;
+    int col;
+};
+
+
+#endif /* MODE_H */
+
